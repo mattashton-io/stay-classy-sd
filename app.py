03cf78a304df61f61acfdeab3ddb156f45ae4c59
Matt Ashton <mattashton@google.com>
2025-12-09 22:52:01 +0000

    updated port number

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def get_news_three():
         return jsonify({'error': str(e)})
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=8080)
